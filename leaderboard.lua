3eb610c63185f56432f2270448c108dca2761ae3
MichaelFromIsrael <michaelbehar99@gmail.com>
2024-02-26 20:31:34 +0200

    4th_commit_added_avatar_images_in_leaderboards

--- leaderboard.lua
+++ leaderboard.lua
@@ -2,9 +2,7 @@ require "class"
 require "common"
 require "logs"
 
-
-
-function ConvertToLuaTable(jsonString) --converts JSON object to lua table
+function ConvertToLuaTable(jsonString) -- converts JSON object to lua table
     -- Remove the brackets at the start and end of the JSON string
     jsonString = jsonString:sub(2, -2)
 
@@ -19,14 +17,14 @@ function ConvertToLuaTable(jsonString) --converts JSON object to lua table
     local users = {}
     for _, entry in ipairs(entries) do
         local user = {}
-        
+
         -- Extract and assign each field to the user table
         for key, value in entry:gmatch('"([^"]+)":"?([^",}]+)"?') do
             if key and value then
                 -- Convert JSON keys to Lua table fields
                 key = key:gsub('avatar_url', 'avatar_url')
                 value = value:gsub('\\/', '/')
-                
+
                 -- Attempt to convert numeric values
                 if tonumber(value) then
                     user[key] = tonumber(value)
@@ -35,19 +33,13 @@ function ConvertToLuaTable(jsonString) --converts JSON object to lua table
                 end
             end
         end
-        
+
         -- Add the parsed user to the users table
         table.insert(users, user)
     end
-    
+
     return users
 end
-
-
-
-
-
-
 
 function FormatLapTime(milliseconds) ---formatting the lap time to a string
     local minutes = math.floor(milliseconds / 60000)
@@ -55,11 +47,6 @@ function FormatLapTime(milliseconds) ---formatting the lap time to a string
     local millis = milliseconds % 1000
     return string.format("%02d:%02d.%03d", minutes, seconds, millis)
 end
-
-
-
-
-
 
 Leaderboard = class()
 ---@class Leaderboard
@@ -71,45 +58,57 @@ function Leaderboard:init(textColor)
 
 end
 
-
 function Leaderboard:addRow(...)
     table.insert(self.rows, {...})
 end
 
-function Leaderboard:fetch() --fetches the data from the API
+function Leaderboard:fetch() -- fetches the data from the API
     self.rows = {}
-    web.get('https://iconx.world/events/get_lap_times.php?event_id=685&num_to_show=5'
-    , function(err, response)
+    web.get('https://iconx.world/events/get_lap_times.php?event_id=685&num_to_show=5', function(err, response)
         if err then
             LogError("Error fetching data: " .. err)
             return
         else
+            self:addRow("avatar", "Name", "Date", "Lap Time")
             local parsedData = ConvertToLuaTable(response.body)
             for i, item in ipairs(parsedData) do
                 -- Assuming item has 'name', 'date', 'lapTime' fields
-                self:addRow(item.name, item.date, item.lap_time)
+                self:addRow(item.avatar_url, item.name, item.date, FormatLapTime(item.lap_time))
             end
         end
     end)
 end
 
-function Leaderboard:render(area)   --rendering function w Area instance of overlay parameter
-    local startX = area.x1 
-    local startY = area.y1 
-    local lineHeight = (area:height())/#self.rows -- The height of each line in the leaderboard
+function Leaderboard:render(area) -- rendering function w Area instance of overlay parameter
+    local startX = area.x1
+    local startY = area.y1
+    local lineHeight = (area:height()) / #self.rows -- The height of each line in the leaderboard
 
     -- Loop through the leaderboard entries and draw them
     for i, user in ipairs(self.rows) do
         -- Calculate the Y position of the current line
         local lineY = startY + (i - 1) * lineHeight
 
+        if(i ==1) then
+            ui.drawText(user[1], vec2(startX, lineY), self.textColor)
+        else
+            if ui.isImageReady(user[1]) then
+
+                    ui.drawImageQuad(user[1], vec2(startX, lineY), vec2(startX  + lineHeight*9/10, lineY), vec2(startX  + lineHeight*9/10, lineY + lineHeight*9/10), vec2(startX, lineY+lineHeight*9/10))
+            else
+                LogDebugPeriodically("du_wi", 3, "Waiting for avatar image...")
+                return
+            end
+        end
+            
         -- Draw the user's name
-        ui.drawText(user[1], vec2(startX, lineY), self.textColor)
+        ui.drawText(user[2], vec2(startX + (area:width()) / 4 - area:width()/10, lineY), self.textColor)
 
         -- Draw the user's date
-        ui.drawText(user[2], vec2(startX + (area:width())/3, lineY), self.textColor)
+        ui.drawText(user[3], vec2(startX + 2 * area:width() / 4, lineY), self.textColor)
 
         -- Draw the user's lap time
-        ui.drawText( FormatLapTime(user[3]),vec2(startX + 2*(area:width())/3, lineY),self.textColor)
+        ui.drawText(user[4], vec2(startX + 3 * area:width() / 4 + area:width()/10, lineY), self.textColor)
+        
     end
 end
